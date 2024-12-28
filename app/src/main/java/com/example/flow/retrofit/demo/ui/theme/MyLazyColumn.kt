516dbf075b957dc68c5a903b740be2f2e4fb4454
shamim-emon <emon.dev.bd@gmail.com>
2024-12-29 00:38:27 +0600

    1. LazyColumn and LazyRow

--- app/src/main/java/com/example/flow/retrofit/demo/ui/theme/MyLazyColumn.kt
+++ app/src/main/java/com/example/flow/retrofit/demo/ui/theme/MyLazyColumn.kt
@@ -1,5 +1,6 @@
 package com.example.flow.retrofit.demo.ui.theme
 
+import androidx.compose.foundation.gestures.ScrollableState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,9 +9,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
+import androidx.compose.foundation.lazy.grid.LazyGridState
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
@@ -22,6 +25,7 @@ import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.distinctUntilChanged
 import kotlinx.coroutines.flow.distinctUntilChanged
 import timber.log.Timber
 
@@ -31,6 +35,25 @@ enum class LayoutType {
     LazyVerticalGrid,
     LazyHorizontalGrid
 }
+
+@Composable
+fun <T> MyScrollable(
+    modifier: Modifier = Modifier,
+    items: List<T>,
+    isLoading: Boolean,
+    layoutType: LayoutType,
+    noOfRowOrColumn: Int = 1,
+    loadMoreItem: () -> Unit = {},
+    content: @Composable (T) -> Unit
+) {
+   when(layoutType){
+       LayoutType.LazyRow->{}
+       LayoutType.LazyColumn -> {}
+       LayoutType.LazyVerticalGrid -> {}
+       LayoutType.LazyHorizontalGrid -> {}
+   }
+}
+
 
 @Composable
 fun <T> MyLazyColumn(
@@ -65,15 +88,7 @@ fun <T> MyLazyColumn(
     }
     // Detect when the user scrolls to the end
     LaunchedEffect(lazyListState, items) {
-        snapshotFlow { lazyListState.layoutInfo.visibleItemsInfo.lastOrNull()?.index }
-            .distinctUntilChanged()
-            .collect { lastVisibleItemIndex ->
-                Timber.tag("EMON1234")
-                    .d("lastVisibleItemIndex:$lastVisibleItemIndex | items.lastIndex:${items.lastIndex}")
-                if (lastVisibleItemIndex == items.lastIndex && !isLoading) {
-                    loadMoreItem.invoke()
-                }
-            }
+        observeLastVisibleItemAndLoadMoreCollect(state = lazyListState, items = items, isLoading = isLoading, loadMoreItem = loadMoreItem)
     }
 
 }
@@ -111,15 +126,7 @@ fun <T> MyLazyRow(
     }
     // Detect when the user scrolls to the end
     LaunchedEffect(lazyListState, items) {
-        snapshotFlow { lazyListState.layoutInfo.visibleItemsInfo.lastOrNull()?.index }
-            .distinctUntilChanged()
-            .collect { lastVisibleItemIndex ->
-                Timber.tag("EMON1234")
-                    .d("lastVisibleItemIndex:$lastVisibleItemIndex | items.lastIndex:${items.lastIndex}")
-                if (lastVisibleItemIndex == items.lastIndex && !isLoading) {
-                    loadMoreItem.invoke()
-                }
-            }
+        observeLastVisibleItemAndLoadMoreCollect(state = lazyListState, items = items, isLoading = isLoading, loadMoreItem = loadMoreItem)
     }
 
 }
@@ -160,15 +167,7 @@ fun <T> MyLazyVerticalGrid(
 
     // Detect when the user scrolls to the end
     LaunchedEffect(lazyGridState, items) {
-        snapshotFlow { lazyGridState.layoutInfo.visibleItemsInfo.lastOrNull()?.index }
-            .distinctUntilChanged()
-            .collect { lastVisibleItemIndex ->
-                Timber.tag("EMON1234")
-                    .d("lastVisibleItemIndex:$lastVisibleItemIndex | items.lastIndex:${items.lastIndex}")
-                if (lastVisibleItemIndex == items.lastIndex && !isLoading) {
-                    loadMoreItem.invoke()
-                }
-            }
+        observeLastVisibleItemAndLoadMoreCollect(state = lazyGridState, items = items, isLoading = isLoading, loadMoreItem = loadMoreItem)
     }
 
 }
@@ -187,7 +186,7 @@ fun <T> MyLazyHorizontalGrid(
     LazyHorizontalGrid(
         rows = GridCells.Fixed(noOfRows),
         state = lazyGridState,
-        modifier = Modifier.fillMaxSize(),
+        modifier = modifier.fillMaxSize(),
         contentPadding = PaddingValues(16.dp), // Add padding around the grid
         horizontalArrangement = Arrangement.spacedBy(16.dp), // Spacing between columns
         verticalArrangement = Arrangement.spacedBy(16.dp) // Spacing between rows
@@ -209,16 +208,36 @@ fun <T> MyLazyHorizontalGrid(
 
     // Detect when the user scrolls to the end
     LaunchedEffect(lazyGridState, items) {
-        snapshotFlow { lazyGridState.layoutInfo.visibleItemsInfo.lastOrNull()?.index }
-            .distinctUntilChanged()
-            .collect { lastVisibleItemIndex ->
-                Timber.tag("EMON1234")
-                    .d("lastVisibleItemIndex:$lastVisibleItemIndex | items.lastIndex:${items.lastIndex}")
-                if (lastVisibleItemIndex == items.lastIndex && !isLoading) {
-                    loadMoreItem.invoke()
-                }
-            }
+        observeLastVisibleItemAndLoadMoreCollect(state = lazyGridState, items = items, isLoading = isLoading, loadMoreItem = loadMoreItem)
     }
 
+}
+
+private suspend fun<T>observeLastVisibleItemAndLoadMoreCollect(
+    state: ScrollableState,
+    items: List<T>,
+    isLoading: Boolean,
+    loadMoreItem: () -> Unit
+) {
+    when(state){
+       is LazyGridState->{
+           snapshotFlow { state.layoutInfo.visibleItemsInfo.lastOrNull()?.index }
+               .distinctUntilChanged()
+               .collect { lastVisibleItemIndex ->
+                   if (lastVisibleItemIndex != null && lastVisibleItemIndex == items.lastIndex && !isLoading) {
+                       loadMoreItem.invoke()
+                   }
+               }
+       }
+       is LazyListState->{
+           snapshotFlow { state.layoutInfo.visibleItemsInfo.lastOrNull()?.index }
+               .distinctUntilChanged()
+               .collect { lastVisibleItemIndex ->
+                   if (lastVisibleItemIndex != null && lastVisibleItemIndex == items.lastIndex && !isLoading) {
+                       loadMoreItem.invoke()
+                   }
+               }
+       }
+    }
 }
 
